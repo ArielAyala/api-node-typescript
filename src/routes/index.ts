4c97f29570b26d4dd288670d4f93cf7d6c372eb8
Ariel Ayala <arielayalamail@gmail.com>
2022-12-31 15:45:59 -0300

    Dynamic routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,19 @@ import { readdirSync } from 'fs';
 const PATH_ROUTER = `${__dirname}`
 const router = Router();
 
+const cleanFileName = (fileName: string) => {
+    const file = fileName.split('.').shift();
+    return file;
+}
+
 readdirSync(PATH_ROUTER).filter((fileName) => {
-    console.log(fileName);
+    const cleanName = cleanFileName(fileName);
+    if (cleanName !== 'index') {
+        import(`./${cleanName}`).then((moduleRouter) => {
+            console.log(`Loading route...${cleanName}`); 
+            router.use(`${cleanName}`, moduleRouter.router);
+        });
+    }
 });
 
 export { router };
--- src/routes/item.ts
+++ src/routes/item.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/items', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
     res.send({ data: 'Test'});
 })
 
